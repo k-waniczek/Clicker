c40f5eac9b271ee3a7a4340eafa735e2666c3b4c
Frezi2005 <kamil.wan05@gmail.com>
2021-11-18 14:52:19 +0100

    Made simple modifiers, yet to add increasing cost

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let clicker = document.querySelector("div#clicker");
-    let coinsSpan = document.querySelector("span#coins");
-    let cpsSpan = document.querySelector("span#cps");
-    let shopBtn = document.querySelector("div#shopBtn");
+    const clicker = document.querySelector("div#clicker");
+    const coinsSpan = document.querySelector("span#coins");
+    const cpsSpan = document.querySelector("span#cps");
+    const shopBtn = document.querySelector("div#shopBtn");
+    let shopModal = document.querySelector("div#shopModal")
     let coins = 0;
     let cps = 0;
     let cpc = 1;
-    let modifiers = {
+    const modifiers = {
         autoclicker: {
             cost: 10,
             cps: 1,
@@ -23,6 +24,27 @@ document.addEventListener("DOMContentLoaded", function() {
             cpc: 2,
         }
     }
+
+    let ownedModifiers = {
+        autoclicker: 5,
+        farm: 0,
+        golden_mouse: 10
+    }
+
+    for(let modifier in modifiers) {
+        shopModal.innerHTML += "<div class='modifier'><span>" + modifier + "</span><span>Cost: " + modifiers[modifier].cost + "</span><span>CPS: " + modifiers[modifier].cps + "</span><span>CPC: " + modifiers[modifier].cpc + "</span><button class='buyBtn' data-modifier='" + modifier + "'>Buy</button></div>";
+        //console.log(modifier + ": Cost: " + modifiers[modifier].cost + ", Clicks per second: " + modifiers[modifier].cps + ", Coins per click: " + modifiers[modifier].cpc)
+    }
+
+    document.querySelectorAll("button.buyBtn").forEach(function(n) {
+        n.addEventListener("click", function() {
+            if(coins >= modifiers[n.getAttribute("data-modifier")].cost) {
+                coins -= modifiers[n.getAttribute("data-modifier")].cost;
+                cps += modifiers[n.getAttribute("data-modifier")].cps;
+                cpc += modifiers[n.getAttribute("data-modifier")].cpc;
+            }
+        })
+    });
 
     clicker.addEventListener("click", function() {
         coinsSpan.innerText = "Coins: " + (coins += cpc);
